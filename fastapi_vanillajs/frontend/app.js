bf5963187e55ba654f9adb68ec0e169fba87f7a2
PVNPUN <92521306+JeonGunwoo@users.noreply.github.com>
2025-05-07 00:03:10 +0900

    .

--- fastapi_vanillajs/frontend/app.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("helloBtn").addEventListener("click", async () => {
-  const response = await fetch("/api/hello");
-  const data = await response.json();
-  document.getElementById("result").textContent = data.message;
-});
